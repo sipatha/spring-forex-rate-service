7d1194c4684d4a6bf3eea37ca8b180e15c6bc53a
n002213f <dev@shoko.co.za>
2018-07-20 07:50:30 +0200

    - don;t start the container - remove dangling images

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-docker ps -a -q --filter="name=forex-rate-service"
+#docker ps -a -q --filter="name=forex-rate-service"
 mvn clean install
-docker build --build-arg JAR_FILE=target/forex-rate-service-0.0.1-SNAPSHOT.jar --file=Dockerfile --tag=spring-demo/forex-rate-service:latest .
-docker run --network=spring-demo -i -t -d spring-demo/forex-rate-service
+docker build --rm=true --build-arg JAR_FILE=target/forex-rate-service-0.0.1-SNAPSHOT.jar --file=Dockerfile --tag=spring-demo/forex-rate-service:latest .
+docker rmi $(docker images -f 'dangling=true' -q)
+# docker run --network=spring-demo -i -t -d spring-demo/forex-rate-service
